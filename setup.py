9426855b2fe78e06d3fdabbfd1137150fed3027d
gatumbac <gatumbac@espol.edu.ec>
2025-10-22 21:20:58 -0500

    Initial commit

---
+++ setup.py
@@ -0,0 +1,30 @@
+import os
+
+from setuptools import setup, find_packages
+
+def readme() -> str:
+    """Utility function to read the README.md.
+
+    Used for the `long_description`. It's nice, because now
+    1) we have a top level README file and
+    2) it's easier to type in the README file than to put a raw string in below.
+
+    Args:
+        nothing
+
+    Returns:
+        String of readed README.md file.
+    """
+    return open(os.path.join(os.path.dirname(__file__), 'README.md')).read()
+
+setup(
+    name='housing_price_prediction',
+    version='0.1.0',
+    author='Team2PreCIAP',
+    author_email='ciap@espol.edu.ec',
+    description='Build and evaluate a regression model that predicts home prices from a real data set.',
+    python_requires='>=3',
+    url='',
+    packages=find_packages(),
+    long_description=readme(),
+)
